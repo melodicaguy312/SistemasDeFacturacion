e61f46e23d907f12fa6daa907c00d5ebfe0d8c1a
melodicaguy312 <melodicaguy312@gmail.com>
2026-04-23 16:57:31 +0200

    RUTA Y UPDATE

--- SistemaDeFacturacion/SistemaDeFacturacion/obj/Debug/net8.0/SistemaDeFacturacion.AssemblyInfo.cs
+++ SistemaDeFacturacion/SistemaDeFacturacion/obj/Debug/net8.0/SistemaDeFacturacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeFacturacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13681273f52a10dcfce99fba4aceecd2b200238e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e603b52d5363ac9614fb4fc8b0ff66bc5a7cfa56")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeFacturacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeFacturacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
